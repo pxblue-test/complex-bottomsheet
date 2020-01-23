df9a8f54c211f61a1279bfdc5b09e7290bf517f6
NikhilBehari <nikhilbehari@gmail.com>
2020-01-22 20:21:25 -0500

    Add passing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
 
   let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
@@ -18,9 +21,10 @@ describe('AppComponent', () => {
     platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
 
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      imports: [AppModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
+
         { provide: StatusBar, useValue: statusBarSpy },
         { provide: SplashScreen, useValue: splashScreenSpy },
         { provide: Platform, useValue: platformSpy },
@@ -42,6 +46,54 @@ describe('AppComponent', () => {
     expect(splashScreenSpy.hide).toHaveBeenCalled();
   });
 
-  // TODO: add more tests!
+  it('should call showBottomSheet() when clicking the top right icon', () => {
+    const fixture = TestBed.createComponent(AppComponent);  
+    const app = fixture.debugElement.componentInstance;    
+    fixture.detectChanges();
+      const showBottomSheetSpy = spyOn(app, 'showBottomSheet').and.stub();
+      const buttonElt = fixture.nativeElement.querySelector('.toolbar-button');
+      buttonElt.click();
+      expect(showBottomSheetSpy).toHaveBeenCalled();
+  });
+
+  it('should display the bottom sheet when clicking the top right icon', ( (done) => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;    
+      app.showBottomSheet().afterOpened().subscribe(() => {
+          const bottomSheet = document.getElementsByClassName('bottomPanel')[0];
+          expect(bottomSheet).toBeTruthy();
+          done();
+      });
+  }));
+
+  it('should render menu items in the bottom sheet', ((done) => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;  
+    fixture.detectChanges();
+      app.showBottomSheet().afterOpened().subscribe(() => {
+          const menuItem = document.getElementById('bottom-panel-item-1');
+          expect(menuItem).toBeTruthy();
+          const menuItem2 = document.getElementById('bottom-panel-item-2');
+          expect(menuItem2).toBeTruthy();
+          done();
+      });
+  }));
+
+  it('should cancel the bottom sheet when clicking on the overlay', (done) => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+      const bottomSheetRef = app.showBottomSheet();
+      bottomSheetRef.afterDismissed().subscribe(() => {
+          const bottomSheet = document.getElementsByClassName('bottomPanel')[0];
+          expect(bottomSheet).toBeFalsy();
+          done();
+      });
+      const overlayClass = 'cdk-overlay-backdrop';
+      const overlay = document.getElementsByClassName(overlayClass)[0] as HTMLElement;
+      overlay.click();
+  });
+    
+  
 
 });
